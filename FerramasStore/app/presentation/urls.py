9fb6f066ba20c0f915edbe0c152838f3f8bad3e0
Juan Andres <jandres.candia18@gmail.com>
2025-05-26 17:56:45 -0400

    Revert "Avance API dolar"
    This reverts commit 428b68074cfba3ce54622c7b8c50aeb8a6511488.

--- FerramasStore/app/presentation/urls.py
+++ FerramasStore/app/presentation/urls.py
@@ -5,7 +5,6 @@ from FerramasStore.app.presentation import views
 router = routers.DefaultRouter()
 router.register(r'productos', views.ProductoViewSet)
 router.register(r'categorias', views.CategoriaViewSet)
-from .views import DolarAPIView
 
 urlpatterns = [
     # Rutas para las páginas del sitio web
@@ -20,5 +19,4 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     # Rutas de la API
     path('api/', include(router.urls)),
-    path('dolar/', DolarAPIView.as_view(), name='dolar-api'),
 ]
